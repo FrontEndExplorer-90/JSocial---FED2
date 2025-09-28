a66d9135e3285934bfac5336f1e1e1bc1b539766
FrontEndExplorer-90 <janina_mey@hotmail.com>
2025-09-28 21:33:55 +0200

    changes in structure

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,41 +1,42 @@
 // src/js/router/index.js
-export default async function router(pathname = window.location.pathname) {
-  
-  const p = pathname.replace(/\/+$/, '') || '/';
+export default async function router(evtOrPath) {
+  let pathname = typeof evtOrPath === 'string' ? evtOrPath : window.location.pathname;
+
+  // normalize: strip trailing slashes, /index.html, and .html
+  let p = pathname
+    .replace(/\/+$/, '')
+    .replace(/\/index\.html$/i, '')
+    .replace(/\.html$/i, '') || '/';
 
   switch (p) {
     case '/':
-      await import('./views/home.js');
+      (await import('./views/home.js')).default();
       break;
-
-    case '/auth':
-      await import('./views/auth.js');
-      break;
-    case '/auth/login':
-      await import('./views/login.js');
-      break;
-    case '/auth/register':
-      await import('./views/register.js');
-      break;
-
     case '/post':
-      await import('./views/post.js');
+      (await import('./views/post.js')).default();
       break;
     case '/post/create':
-      await import('./views/postCreate.js');
+      (await import('./views/postCreate.js')).default();
       break;
     case '/post/edit':
-      await import('./views/postEdit.js');
+      (await import('./views/postEdit.js')).default();
       break;
-    case '/post/details':              
-      await import('./views/postDetails.js');
+    case '/post/details':
+      (await import('./views/postDetails.js')).default();
       break;
-
+    case '/auth':
+      (await import('./views/auth.js')).default();
+      break;
+    case '/auth/login':
+      (await import('./views/login.js')).default();
+      break;
+    case '/auth/register':
+      (await import('./views/register.js')).default();
+      break;
     case '/profile':
-      await import('./views/profile.js');
+      (await import('./views/profile.js')).default();
       break;
-
     default:
-      await import('./views/notFound.js');
+      (await import('./views/notFound.js')).default?.();
   }
 }
