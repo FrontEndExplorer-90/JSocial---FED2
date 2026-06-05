e34a98c909445cc1c97db295e04e0a38aba09a54
FrontEndExplorer-90 <janina_mey@hotmail.com>
2026-06-05 21:42:05 +0200

    Fix router view exports

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,6 +1,6 @@
 import { authGuard } from "../../utilities/authGuard.js";
 import "../../ui/auth/logout.js";
-import "../ui/profile/profile.js";
+import "../../ui/profile/profile.js";
 
 export default function () {
   authGuard();
